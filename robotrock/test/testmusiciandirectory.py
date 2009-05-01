a768c7ef1f762b941fb909f2fc46548eac55eecf
beenen34 <beenen34@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-01 00:09:21 +0000

    renamed and modified test/testmusiciandirectory.py to work with new file names
    git-svn-id: https://subversion.assembla.com/svn/cse403@11 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/test/testmusiciandirectory.py
+++ robotrock/test/testmusiciandirectory.py
@@ -1,11 +1,13 @@
-import MusicianDirectory
 import unittest
+import sys
 
-class TestMusicianDirectoryFunctions(unittest.TestCase):
+class TestMusicianDirectory(unittest.TestCase):
 
     def testfilter_musician_list(self):
         list = MusicianDirectory.filter_musician_list(['acoustic'])
         self.assertEqual(['acoustic guitar'], list)
         
 if __name__ == '__main__':
+    sys.path.append('../robotrock/')
+    import musiciandirectory
     unittest.main()
