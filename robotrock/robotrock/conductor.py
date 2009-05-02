71f4ffeea260c5fb303b0c077d09d343e2d4f0b2
uniquesnowflake8 <uniquesnowflake8@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-02 01:25:37 +0000

    Conductor tests now pass.
    git-svn-id: https://subversion.assembla.com/svn/cse403@21 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/conductor.py
+++ robotrock/robotrock/conductor.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
 ''' conductor.py
     Author: Alan Fineberg (af@cs.washington.edu)
-    The conductor is responsible for telling the AI to perform.
+    The conductor is responsible for telling the AI to perform,
+    and specifying their performance order.
     The onPulse() method is triggered for each metronome pulse.
 '''
 class Conductor(object):
     def __init__(self, ensemble):
         self.ensemble = ensemble
+        self.order = list(ensemble)
     
+    # Add a musician to the ensemble, and place it
+    # in the conductor's own internal musician order.
     def addMusician(self, musician):
-        pass
+        self.ensemble.append(musician)
+        self.order.append(musician)
     
+    # Remove the musician from the ensemble.
+    # If the musician does not exist, the 
+    # ValueError exception must be handled.
     def removeMusician(self, musician):
-        pass
+        self.ensemble.remove(musician)
+        self.order.remove(musician)
     
     def onPulse(self, elapsed):
-        for musician in self.ensemble:
+        for musician in self.order:
             musician.compose('duration')
 
 if __name__ == '__main__':
--- robotrock/test/testconductor.py
+++ robotrock/test/testconductor.py
@@ -14,22 +14,34 @@ class TestConductor(unittest.TestCase):
             def __init__(self, id, log):
                 self.id = id
                 self.log = log
-                
+            
             def compose(self, duration):
-                self.log.append(self.id)  
+                self.log.append(self.id)
         
         self.musicians = [Musician(i, self.test_log) for i in range(0, 10)]
         self.conductor = Conductor(self.musicians)
-        
+    
     def testCompose(self):
         self.conductor.onPulse('some_value')
-        [self.assertEqual(i, log_item) for (i, log_item) in enumerate(self.test_log)]
+        [self.assertEqual(i, log_item) for (i, log_item) in \
+                enumerate(self.test_log)]
     
     def testAddMusician(self):
-        self.assertTrue(False)
+        m = ['stub musician object']
+        self.conductor.addMusician(m)
+        self.assertTrue(m in self.conductor.ensemble)
+        self.assertTrue(m in self.conductor.order)
     
     def testRemoveMusician(self):
-        self.assertTrue(False)
+        m = self.musicians[5]
+        self.conductor.removeMusician(m)
+        self.assertFalse(m in self.conductor.ensemble)
+        self.assertFalse(m in self.conductor.order)
+    
+    def testConductorFollowsOrder(self):
+        self.conductor.order = reversed(self.musicians)
+        [self.assertEqual(i, log_item) for (i, log_item) in \
+                enumerate(reversed(self.test_log))]
 
 if __name__ == '__main__':
     sys.path.append('../robotrock/')
