2364f2dfc76d782ed8ebcc540862631b42828d28
uniquesnowflake8 <uniquesnowflake8@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-09 20:52:08 +0000

    Updated conductor to keep track of the current measure for each musician
    git-svn-id: https://subversion.assembla.com/svn/cse403@37 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/conductor.py
+++ robotrock/robotrock/conductor.py
@@ -5,27 +5,86 @@
     and specifying their performance order.
     The onPulse() method is triggered for each metronome pulse.
 '''
+import staff
+
 class Conductor(object):
-    def __init__(self, ensemble):
+    def __init__(self, score, songInfo):
+        ''' 
+        Create a conductor with a score, and a pointer to the current song
+        information. The conductor is responsible for cueing each musician
+        to play their respective measures, and must keep the measure
+        meta data up to date for the musicians.
+        '''
+        self.score = score
         self.ensemble = []
-        self.order = list(ensemble)
+        self.current_musician_measures = {}
+        self.song_info = songInfo
+        self.measure_info = songInfo.measureInfo()
+        # Chunks allows us to keep our place within a measure.
+        self.__chunks = 0
+        # Fixme: These shouldn't be hardcoded. Load from a file.
+        self.__chunk_values = {'tick': 1}
+        self.__chunks_per_beat = 192
     
-    # Add a musician to the ensemble, and place it
-    # in the conductor's own internal musician order.
     def addMusician(self, musician):
+        ''' Adds a musician and associates them with a staff, mapped one-to-one. '''
         self.ensemble.append(musician)
-        self.order.append(musician)
+        self.__appendStaff(self.score, musician)
     
-    # Remove the musician from the ensemble.
-    # If the musician does not exist, the 
-    # ValueError exception must be handled.
+    def __appendStaff(self, score, musician):
+        score.staffs.append(staff.Staff(instrument=musician.instrument))
+        # Assume no other entity adding or removing staffs (or single threaded).
+        self.current_musician_measures[musician] = \
+                self.__firstMeasureFromStaff(score.staffs[-1])
+        
+    def __firstMeasureFromStaff(self, staff):
+        return staff.measures.next()
+        
     def removeMusician(self, musician):
         self.ensemble.remove(musician)
-        self.order.remove(musician)
     
     def onPulse(self, elapsed):
-        for musician in self.order:
-            musician.play(elapsed)
+        ''' 
+        On every metronome tick, we check to see if we have reached the end of
+        our current measure, and tell each musician to performa a measure
+        with its metadata up to date.
+        '''
+        newMeasure = self.__isNewMeasure(elapsed)
+        
+        # Update the measure info to reflect what's currently in song info.
+        if newMeasure:
+            self.measure_info = self.song_info.measureInfo()
+            for musician in self.ensemble:
+                self.__advanceMeasure(musician)
+
+        for (musician, measure) in self.current_musician_measures:
+            musician.compose(measure, elapsed)
+    
+    def __isNewMeasure(self, elapsed):
+        ''' 
+        Keep track of how far along we are in each measure. If we've reached
+        the last beat, then reset __chunks so that the next pulse triggers
+        the start of a new measure.
+        '''
+        result = self.__chunks == 0
+        
+        # Returns the numerator of the time signature. Use our own 
+        # internal copy of the measure_info, instead of what the user
+        # might have modified.
+        beatsInMeasure = self.measure_info['time_signature'][0]
+        self.__chunks += self.__chunk_values[elapsed]
+        
+        if self.__chunks == self.__chunks_per_beat * beatsInMeasure:
+            self.__chunks = 0
+
+        return result
+        
+    def __advanceMeasure(self):
+        measure = self.current_musician_measures[musician]
+        self.current_musician_measures[musician] = measure.next()
+
+    def __updateMeasureInfo(self, measure, measureInfo):
+        measure.__dict__.update(measureInfo)
 
 if __name__ == '__main__':
     conductor = Conductor(['musician1', 'musician2'])
--- robotrock/test/testconductor.py
+++ robotrock/test/testconductor.py
@@ -12,31 +12,49 @@ class TestConductor(unittest.TestCase):
         
         class Musician(object):
             def __init__(self, id, log):
+                ''' id is used only for testing purposes. '''
                 self.id = id
                 self.log = log
+                self.instrument = 'Jazz Flute'
             
             def compose(self, duration):
                 self.log.append(self.id)
         
+        class SongInfo(object):
+            def measureInfo(self):
+                return dict(time_signature=(4, 4))
+        
+        class Staff(dict):
+            pass
+
+        class Score(object):
+            def __init__(self):
+                self.staffs = expandinglist.ExpandingList(Staff)
+        
         self.musicians = [Musician(i, self.test_log) for i in range(0, 10)]
-        self.conductor = Conductor(self.musicians)
+        self.flautist = Musician(id='flautist', log='')
+        self.score = Score()
+        self.song_info = SongInfo()
+        self.conductor = conductor.Conductor(self.score, self.song_info)
     
     def testCompose(self):
-        self.conductor.onPulse('some_value')
+        ''' Each musician should write to their log items when compose is called. '''
+        self.conductor.onPulse('tick')
         [self.assertEqual(i, log_item) for (i, log_item) in \
                 enumerate(self.test_log)]
     
     def testAddMusician(self):
-        m = ['stub musician object']
-        self.conductor.addMusician(m)
-        self.assertTrue(m in self.conductor.ensemble)
-        self.assertTrue(m in self.conductor.order)
-    
+        ''' Adding a musician appends the ensemble and the staff list. '''
+        self.conductor.addMusician(self.flautist)
+        self.assertTrue(self.flautist in self.conductor.ensemble)
+        self.assertEquals(len(self.score.staffs), 1)
+        
     def testRemoveMusician(self):
         m = self.musicians[5]
+        self.conductor.addMusician(m)
+        self.assertTrue(m in self.conductor.ensemble)
         self.conductor.removeMusician(m)
         self.assertFalse(m in self.conductor.ensemble)
-        self.assertFalse(m in self.conductor.order)
     
     def testConductorFollowsOrder(self):
         self.conductor.order = reversed(self.musicians)
@@ -45,5 +63,6 @@ class TestConductor(unittest.TestCase):
 
 if __name__ == '__main__':
     sys.path.append('../robotrock/')
-    from conductor import Conductor
+    import conductor
+    import expandinglist
     unittest.main()
