50fa51bec4a27f99aab253ee40296d4d85aae2a8
uniquesnowflake8 <uniquesnowflake8@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-10 23:32:15 +0000

    Added note values to the note class
    git-svn-id: https://subversion.assembla.com/svn/cse403@46 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/note.py
+++ robotrock/robotrock/note.py
@@ -6,8 +6,27 @@
 '''
 class Note(object):
     ''' note_info is a dictionary with a Note's attributes fully described. '''
+    note_values = NoteValues()
     def __init__(self, **note_info):
         self.__dict__.update(note_info)
+
+class NoteValues(object):
+        def __init__(self):
+            self.SIXTYFOURTH_NOTE = 6
+            self.THIRTYSECOND_NOTE = 2 * self.SIXTY_FOURTHNOTE
+            self.SIXTEENTH_NOTE = 2 * self.THIRTYSECOND_NOTE
+            self.EIGHT_NOTE = 2 * self.SIXTEENTH_NOTE
+            self.QUARTER_NOTE = 2 * self.SIXTEENTH_NOTE
+            self.HALF_NOTE = 2 * self.QUARTER_NOTE
+            self.WHOLE_NOTE = 2 * self.HALF_NOTE
+            
+            self.QUARTER_NOTE_TRIPLET = self.WHOLE_NOTE / 3
+            self.EIGTH_NOTE_TRIPLET =  self.QUARTER_NOTE / 3
+            self.SIXTEENTH_NOTE_TRIPLET = self.SIXTEENTH_NOTE / 3
+            self.THIRTYSECOND_NOTE_TRIPLET = self.THIRTYSECOND_NOTE / 3
         
+        def dotted(value):
+            return 3 * value // 2
+            
 if __name__ == '__main__':
     note = Note(tone=('A',5), duration=('quarter'), dynamic='mezzoforte')
