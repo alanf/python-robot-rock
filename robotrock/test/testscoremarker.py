bec5ad96d70f55a785b9854d4b158eac04808d13
veraltb <veraltb@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-13 23:14:16 +0000

    Corrected blatant errors. Ready for ScoreMarker implementation.
    git-svn-id: https://subversion.assembla.com/svn/cse403@57 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/test/testscoremarker.py
+++ robotrock/test/testscoremarker.py
@@ -1,35 +1,41 @@
-# testscoremarker.py
+''' testscoremarker.py
+    Author: Travis Veralrud <veralrud@cs.washington.edu>
+    Unit tests for ScoreMarker.
+'''
 
 import unittest
 import sys
 sys.path.append('../robotrock')
 import note
+from score import *
 
 from scoremarker import ScoreMarker
 
-class TestScoreMarker(unittest.TestUnit):
+class TestScoreMarker(unittest.TestCase):
 
 	def testGetNotes(self):
+		"""Tests expected results from ScoreMarker.getNotes() method."""
 
 		# Add quarter notes at beats 1 and 2.
 
-		first_beat = note.Note( start=0.0, duration=note.getValues().QUARTER_NOTE )
-		score = Score[0].measures[0].AddNote( n )
+		first_beat = note.Note( start=0.0, duration=note.NoteValues().QUARTER_NOTE )
+		score = Score()
+		score.staffs[0].measures[0].addNote( first_beat )
 
-		second_beat = note.Note( start=1.0, duration=note.getValues().QUARTER_NOTE )
-		score = Score[0].measures[0].AddNote( n )
+		second_beat = note.Note( start=1.0, duration=note.NoteValues().QUARTER_NOTE )
+		score.staffs[0].measures[0].addNote( second_beat )
 
 		marker = ScoreMarker( score )
 
-		notes = marker.getNotes( note.getValues().EIGHTH_NOTE )
+		notes = marker.getNotes( note.NoteValues().EIGHTH_NOTE )
 
 		# Get one note...
 		self.assertEquals( 1, len( notes ) )
 		self.assertEquals( first_beat, notes[0] )
 
-		# move forward; second note should look like the first
-		marker.forward( note.getValues().QUARTER_NOTE )
-		notes = marker.getNotes( note.getValues().EIGHTH_NOTE )
+		# Move forward one quarter; second note should look like the first
+		marker.forward( note.NoteValues().QUARTER_NOTE )
+		notes = marker.getNotes( note.NoteValues().EIGHTH_NOTE )
 		self.assertEquals( first_beat, notes[0] )
 
 if __name__ == '__main__':
