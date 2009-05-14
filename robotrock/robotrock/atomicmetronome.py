a811c58780ddf79342012e4dc7b6faa35a29a907
veraltb <veraltb@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-14 05:03:50 +0000

    Added AtomicMetronome class. Metronome was cleaned up to be a base class should other implementations arise.
    git-svn-id: https://subversion.assembla.com/svn/cse403@77 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ robotrock/robotrock/atomicmetronome.py
@@ -0,0 +1,52 @@
+''' atomicmetronome.py
+    Author: Travis Veralrud <veralrud@cs.washington.edu>
+    A flavor of Metronome that operates at small, fixed beats.
+'''
+
+import metronome
+from note import Note
+
+# Discrete number of calls per beat. This effectively defines the
+# resolution of the atomic parser.
+# NOTE 2**4 * 3 adds support for 64th standard notes (quarter / 16)
+#      but only eighth triplets (quarter / 3).
+DIVISIONS_PER_QUARTER_NOTE = 2**4 * 3
+# Length between Listener's onPulse() event generation
+ATOMIC_BEAT = 1.0 / DIVISIONS_PER_QUARTER_NOTE
+# Value to report to listeners
+# This value directly corresponds to DIVISIONS_PER_QUARTER_NOTE
+ATOMIC_NOTE = Note.note_values.QUARTER_NOTE // DIVISIONS_PER_QUARTER_NOTE
+
+class AtomicMetronome(metronome.Metronome):
+	"""An object to convert time into beats with respect to a given tempo.
+	This Metronome will generate onPulse events at a fixed rate.
+
+	Listener objects receive onPulse events from a Metronome."""
+
+	def __init__(self, tempo = metronome.DEFAULT_TEMPO):
+		self.listeners = []
+		self.tempo = tempo
+		self.current_beat = ATOMIC_NOTE
+
+	def addListener(self, listener):
+		"""Adds the given listener to receive onPulse() events."""
+		self.listeners.append( listener )
+
+	def advance(self, elapsed_time_in_seconds):
+		"""Advances the metronome's clock by given time and triggers
+		the onPulse events for listeners that registered for pulse
+		events in this time frame.
+
+		Each listener will only be called once in this advancement
+		period, so they may make use of the given elapsed time."""
+
+		elapsed_beats = elapsed_time_in_seconds * self.tempo_in_bps
+		self.current_beat += elapsed_beats
+
+		# Call onPulse for all listeners.
+		if self.current_beat / ATOMIC_BEAT >= 1.0:
+			for listener in self.listeners:
+				listener.onPulse( ATOMIC_NOTE )
+
+		self.current_beat %= ATOMIC_BEAT
+
--- robotrock/robotrock/metronome.py
+++ robotrock/robotrock/metronome.py
@@ -1,12 +1,10 @@
 ''' metronome.py
     Author: Travis Veralrud <veralrud@cs.washington.edu>
+	Defines Metronome, a base class for concrete implementations.
 '''
-
-from heapq import heappush, heappop
 
 DEFAULT_TEMPO = 120 # in beats per minute
 MINIMUM_TEMPO =   1 
-MAXIMUM_TEMPO = 360 # NOTE: arbitrary
 
 class Metronome(object):
 	"""An object to convert time into beats with respect to a given tempo.
@@ -15,59 +13,25 @@ class Metronome(object):
 	the onPulse event, a Listener indicates the next beat it needs the next event."""
 
 	def __init__(self, tempo = DEFAULT_TEMPO):
-		self.listeners = []
-		self.setTempo( tempo )
-		self.heap = []
-		self.current_beat = 0.0
-		# The priority counter keeps track of the priority values given to added listeners.
-		# This priority ensures Listener ordering when two Listeners are called upon
-		# the same beat.
-		self.priority_counter = 0
-	def addListener(self, listener, next_pulse = 0.0):
-		"""Adds the given listener to receive onPulse() events.
-		next_pulse specifies the delay in beat when the listener expects
-		to receive its first event; default is immediate at next
-		call to advance()."""
-		heappush( self.heap, (self.current_beat + next_pulse, self.priority_counter, listener))
-		self.priority_counter += 1
+		self.tempo = tempo
+
+	def addListener(self, listener):
+		"""Adds the given listener to receive onPulse() events."""
+		pass
 
 	def advance(self, elapsed_time_in_seconds):
 		"""Advances the metronome's clock by given time and triggers
 		the onPulse events for listeners that registered for pulse
-		events in this time frame.
+		events in this time frame."""
+		pass
 		
-		Each listener will only be called once in this advancement
-		period, so they may make use of the given elapsed time."""
-
-		elapsed_beats = elapsed_time_in_seconds * self.tempo_in_bps
-
-		self.current_beat += elapsed_beats
-		
-		processed_listeners = []
-
-		# Call onPulse for all expecting listeners once only.
-		while len(self.heap) > 0 and self.heap[0][0] <= self.current_beat:
-			listener = heappop( self.heap )[-1]
-			next_t = listener.onPulse( elapsed_beats )
-			processed_listeners.append( (self.current_beat + next_t, listener) )
-
-		# With processing complete, add listeners back on to heap.
-		for l in processed_listeners:
-			heappush( self.heap, l )
-
-		# FIXME Consider floating point errors!
-		#       Reseting current beat and adjusting listeners will
-		#       solve this issue.
-
 	def setTempo(self, tempo):
 		"Sets the tempo in beats per minute."
-		if tempo < MINIMUM_TEMPO:
-			tempo = MINIMUM_TEMPO
-		elif tempo > MAXIMUM_TEMPO:
-			tempo = MAXIMUM_TEMPO
-		self.tempo_in_bps = tempo / 60.0
+		self.tempo_in_bps = max(tempo, MINIMUM_TEMPO) / 60.0
 
 	def getTempo(self):
 		"Gets the current tempo in beats per minute."
 		return self.tempo_in_bps * 60.0
+
+	tempo = property(fget = getTempo, fset = setTempo, doc = "The tempo in beats per minute.")
 
