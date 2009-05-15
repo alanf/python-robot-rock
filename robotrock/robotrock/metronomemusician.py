132b84b895cd63262d95ea7bc39fa3d039cbb48d
veraltb <veraltb@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-15 23:54:51 +0000

    Updated for tone to be specified by drum component, rather than magic number.
    git-svn-id: https://subversion.assembla.com/svn/cse403@120 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/metronomemusician.py
+++ robotrock/robotrock/metronomemusician.py
@@ -8,13 +8,14 @@ Created by Alan Fineberg on 2009-05-03.
 import sys
 import note
 import dynamics
+from drumkit import DrumKit
 
 class MetronomeMusician(object):
     def __init__(self):
         self.instrument = 'metronome'
     
     def compose(self, measure, window_start, window_duration):
-        myNote = note.Note(tone=42, start=window_start, \
+        myNote = note.Note(tone=DrumKit("closed hi-hat"), start=window_start, \
                 duration=note.Note.note_values.QUARTER_NOTE, \
                 rest=False, dynamic=dynamics.FORTE)
         measure.addNote(myNote)
