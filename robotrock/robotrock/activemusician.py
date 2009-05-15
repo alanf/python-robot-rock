77c0a25d50a2ad352f71d8da5ae4e679227ff067
uniquesnowflake8 <uniquesnowflake8@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-15 02:32:40 +0000

    Active musician is more responisve
    git-svn-id: https://subversion.assembla.com/svn/cse403@113 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/activemusician.py
+++ robotrock/robotrock/activemusician.py
@@ -12,37 +12,53 @@ import dynamics
 class ActiveMusician(object):
     def __init__(self):
         self.instrument = 'metronome'
-        self.energy = 50
-        self.complexity = 50
+        self.__energy = 50
+        self.__complexity = 50
+        self.changed = False
     
     def compose(self, measure, window_start, window_duration):
-	print 'energy', self.energy, 'complex', self.complexity
-        if window_start == 0:
+        print 'energy', self.__energy, 'complex', self.__complexity
+        if window_start == 0 or self.changed:
+            if self.changed:
+                measure.notes = []
+                self.changed = False
+                
             my_duration = note.Note.note_values.QUARTER_NOTE
-            if self.energy > 75:
+            
+            if self.__energy > 60:
                 my_duration = my_duration // 4
-	    elif self.energy > 60:
-		my_duration = my_duration // 2
-            elif self.energy < 25:
-                my_duration = my_duration * 2 // 1
+            elif self.__energy > 50:
+                my_duration = my_duration // 2
+            elif self.__energy < 25:
+                    my_duration = my_duration * 2 // 1
         
             my_start = 0
-            if self.complexity > 30:
-                start = note.Note.note_values.EIGHTH_NOTE
-		my_duration = my_duration * 3 // 2
-            if self.complexity > 60:
+        
+            if self.__complexity > 50:
                 note.Note.note_values.EIGHTH_NOTE_TRIPLET
-		my_duration = my_duration * 3 // 2
+                my_duration = my_duration * 3 // 2
+            elif self.__complexity > 30:
+                start = note.Note.note_values.EIGHTH_NOTE
+                my_duration = my_duration * 3 // 2
         
             total_duration = my_start
-	    i = 0
+            i = 0
             while total_duration < note.Note.note_values.QUARTER_NOTE * 4:
                 myNote = note.Note(tone=42, start=my_start +i*my_duration, \
                         duration=my_duration, \
                         rest=False, dynamic=dynamics.FORTE)
                 measure.addNote(myNote)
                 total_duration += my_duration
-		i += 1
+                i += 1
 
-if __name__ == '__main__':
-    m = MetronomeMusician()
+    def __setComplexity(self, value):
+        self.__complexity = value
+        self.changed = True
+    
+    def __setEnergy(self, value):
+        self.__energy = value
+        self.changed = True
+    
+    complexity = property(fset=__setComplexity)
+    energy = property(fset=__setEnergy)
+
