c1129b41e227d04b70e95bd965ed385a1c19712f
beenen34 <beenen34@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-16 00:46:11 +0000

    added documentation to system test testaddmusician.py
    git-svn-id: https://subversion.assembla.com/svn/cse403@129 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/test/testaddmusician.py
+++ robotrock/test/testaddmusician.py
@@ -21,6 +21,7 @@ import songinfo
 class TestAddMusician(unittest.TestCase):
     
     def setUp(self):
+	# Set up all the necessary objects for using a core contorller and conductor
         self.metronome = atomicmetronome.AtomicMetronome()
         self.audio_driver = audiodriver.AudioDriver(self.metronome)
         self.score_object = score.Score()
@@ -30,8 +31,10 @@ class TestAddMusician(unittest.TestCase):
                 self.metronome, self.conductor_object, self.song_info)
 
     def testaddMusician(self):
+	# Ensure the ensemble starts as empty
 	self.conductor_object.ensemble = []
 	test_musician = musicianstructured.MusicianStructured()
+	# Add test musician and ensure the conductors ensemble is updated accordingly
 	self.core_controller.addMusician(test_musician)
 	self.assertEquals(self.conductor_object.ensemble, [test_musician])
 	
