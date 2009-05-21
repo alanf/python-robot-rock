753225435e139190e3e52ed6e2053570060a57f8
veraltb <veraltb@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-21 21:09:05 +0000

    Implemented class SoundfontDirectory. Passes all current tests.
    git-svn-id: https://subversion.assembla.com/svn/cse403@152 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/soundfontdirectory.py
+++ robotrock/robotrock/soundfontdirectory.py
@@ -15,10 +15,41 @@
 
 class SoundfontDirectory(object):
 
-	def __init__(self):
-		self.instruments = {}
+    def __init__(self):
+        self.instruments = {}
 
-	def load(self, filename):
-		"Loads the instruments from the given filename."
-		pass
+    def load(self, filename):
+        """Loads the instruments from the given filename.
+
+        See module documentation for file format.
+        
+        Throws RuntimeError upon detecting malformed files."""
+
+        f = open(filename)
+
+        for line in f:
+            # Ignore comments
+            line = line.split('#')[0].strip()
+            if ':' not in line: continue
+            key, values = line.split(":")
+            key = key.strip()
+            values = values.strip().split()
+
+            # Bail on malformed files
+            if len( values ) == 0 or len( values ) > 3:
+                raise RuntimeError( "Malformed line:\n\t%s" % (line) )
+
+            # Looks good so far...
+            # FIXME What if bank, patch are not ints?
+            filename = values[0].strip()
+            bank = 0
+            patch = 0
+            if len( values ) == 2:
+                patch = int( values[1] )
+            if len( values ) == 3:
+                bank = int( values[1] )
+                patch = int( values[2] )
+
+            # finally, add to dict
+            self.instruments[key] = ( filename, bank, patch )
 
