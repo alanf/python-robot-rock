224f27c7b3c7703a85edd87a4d8ca01e17c8cb50
veraltb <veraltb@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-23 00:50:36 +0000

    Bug fix: Now it doesn't hog CPU,
    git-svn-id: https://subversion.assembla.com/svn/cse403@182 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/audiodriver.py
+++ robotrock/robotrock/audiodriver.py
@@ -9,48 +9,53 @@ from time import sleep
 from time import time
 
 class AudioDriver(Thread):
-	"""A real-time audio driver.
+    """A real-time audio driver.
 
-	Supply an instantiation of the Metronome class and begin thread with start().
+    Supply an instantiation of the Metronome class and begin thread with start().
 
-	The metronome is initially halted. Use play() to begin."""
+    The metronome is initially halted. Use play() to begin."""
 
-	def __init__(self, clock, metronome):
-		"""Constructor.
+    def __init__(self, clock, metronome):
+        """Constructor.
 
-		clock: mechanism to regulate time. Must conform to Clock interface.
-		metronome: generator of onPulse events. Must conform to Metronome interface. """
+        clock: mechanism to regulate time. Must conform to Clock interface.
+        metronome: generator of onPulse events. Must conform to Metronome interface. """
 
-		Thread.__init__(self)
+        Thread.__init__(self)
 
-		self.clock = clock
-		self.m = metronome
+        self.clock = clock
+        self.m = metronome
 
-		self.running = False
-		self.playing = False
+        self.running = False
+        self.playing = False
 
-	def run(self):
-		"DO NOT CALL DIRECTLY. This will be called by start()."
-		self.running = True
-		last_time = self.clock.time()
-		while self.running:
-			current_time = self.clock.time()
-			if self.playing:
-				self.m.advance( current_time - last_time )
-			# Be nice to CPU and not burn cycles...
-			# sleep( 0.001 ) # BETA: Don't worry about yet.
-			last_time = current_time
+    def run(self):
+        "DO NOT CALL DIRECTLY. This will be called by start()."
+        self.running = True
+        last_time = self.clock.time()
+        sleep_time = last_time % 1.0
+        while self.running:
+            current_time = self.clock.time()
+            sleep_time += current_time
+            if self.playing:
+                self.m.advance( current_time - last_time )
+            else:
+                sleep ( 0.1 )
+            last_time = current_time
+            if sleep_time > 1.0:
+                sleep( 0.0001 )
+            sleep_time %= 1.0
 
-	def halt(self):
-		"""Triggers this audio driver thread to halts this current thread.
-		This may be called from another thread."""
-		self.running = False
+    def halt(self):
+        """Triggers this audio driver thread to halts this current thread.
+        This may be called from another thread."""
+        self.running = False
 
-	def play(self):
-		"Begin metronome onPulse event generation."
-		self.playing = True
+    def play(self):
+        "Begin metronome onPulse event generation."
+        self.playing = True
 
-	def pause(self):
-		"Stop metronome onPulse event generation."
-		self.playing = False
+    def pause(self):
+        "Stop metronome onPulse event generation."
+        self.playing = False
 
