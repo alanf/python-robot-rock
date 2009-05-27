6a488ff42178b97f49ddb3a4a11b4eb7f4f1dfc7
uniquesnowflake8 <uniquesnowflake8@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-27 01:26:02 +0000

    Updated metronome to reset its changed flag and to produce a distinguishing tone on the first beat of each measure. Helps to prove that we respond to key changes.
    git-svn-id: https://subversion.assembla.com/svn/cse403@225 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/robotrock/musicians/metronomemusician/metronome.py
+++ robotrock/robotrock/musicians/metronomemusician/metronome.py
@@ -19,7 +19,7 @@ class Metronome(MusicianStructured):
     def __init__(self, energy=50, complexity=50, time = (4,4), key = ('B', 'major')):
         MusicianStructured.__init__(self, energy, complexity, time, key)
         self.instrument = 'metronome'
-        self._my_tone=DrumKit["claves"]
+        self._my_tone=DrumKit['closed hi-hat']
 
     # This method decides if new music needs to be composed on this iteration.
     # It returns a boolean True when music needs to be composed, or False when
@@ -30,8 +30,12 @@ class Metronome(MusicianStructured):
     # This method decides what will be played this iteration.
     # For a metronome, that is a note on every beat and only every beat.
     def _write(self):
-        for x in range(self._time[0]):
+        # Note 1 has an accent to distinguish the time signature.
+        self._addNote(0, DrumKit['hi wood block'])
+        for x in range(1, self._time[0]):
+            print 'Adding note ', x
             self._addNote(x, self._my_tone)
+        self._changed = False
 
 # Returns the constructor for a Metronome.
 def Musician():
