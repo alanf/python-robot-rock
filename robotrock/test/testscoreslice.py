afc03fa0c8fc55cb148cedd9150ebf54e8efd03d
uniquesnowflake8 <uniquesnowflake8@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-29 00:01:54 +0000

    ScoreSlice no longer needs a test for its copy method, we are using a built in function instead.
    git-svn-id: https://subversion.assembla.com/svn/cse403@260 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- robotrock/test/testscoreslice.py
+++ robotrock/test/testscoreslice.py
@@ -23,18 +23,6 @@ class TestScoreSlice(unittest.TestCase):
         # Checks the result of all 4 score slices against the expected values.
         for (i, word) in enumerate(self.measure_labels):
             [self.assertEqual(word, result) for result in self.score_slice[i]]
-    
-    def testCopy(self):
-        self.score_slice.current_index = 5
-        copy = self.score_slice.copy()
-        
-        self.assertEquals(self.score_slice.current_index, copy.current_index)
-        
-        # Ensure that changing the copy doesn't affect the original.
-        copy.current_index = 123
-        self.assertEquals(self.score_slice.current_index, 5)
-        # Make sure that we are actually pointing to the staff data.
-        self.assertEquals(copy[1][1], 'measure2')
         
 if __name__ == '__main__':
     sys.path.append('../robotrock/')
